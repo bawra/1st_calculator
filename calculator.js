148f9e3e66242f3f1d92de2b95b1398a66007dc2
bawra11 <abhijit.ghosh@mu-sigma.com>
2018-10-22 18:12:08 +0530

    changed appearence and everything

--- calculator.js
+++ calculator.js
@@ -1,42 +1,87 @@
-function myFunction() {
-
-  var nb1 = document.getElementById('number1').value;
-  nb1 = Number(nb1);
-  var nb2 = document.getElementById('number2').value;
-  nb2 = Number(nb2);
-  var action = document.getElementById('action').value;
-
-  console.log(action, nb1, nb2);
-  var result = 1;
-  switch (action) {
-    case "+":
-      {
-        result = (nb1 + nb2);
-        alert("The result of addition is " + result);
-        break;
+const calculator = document.querySelector(".calculator")
+const display = document.querySelector('.calculator__display')
+const action_display = document.querySelector('.action__display')
+const keys = calculator.querySelector(".calculator__keys")
+keys.addEventListener("click", e => {
+  if (e.target.matches("button")) {
+    const key = e.target
+    const action = key.dataset.action
+    if (!action){
+      if (display.textContent == '0' || calculator.dataset.previousKeyType == 'operator'){
+        display.textContent = key.textContent
+        action_display.textContent = action_display.textContent + key.textContent
+        calculator.dataset.previousKeyType = 'deciml'
       }
-    case "-":
-      {
-        result = (nb1 - nb2);
-        alert("The result of substraction is " + result);
-        break;
+      else{
+        display.textContent = display.textContent + key.textContent
+        action_display.textContent = action_display.textContent + key.textContent
+        calculator.dataset.previousKeyType = 'deciml'
       }
-    case "*":
-      {
-        result = (nb1 * nb2);
-        alert("The result of multiplication is " + result);
-        break;
-      }
-    case "/": {
-      result = (nb1 / nb2);
-      alert("The result of division is " + result);
-      break;
     }
-    default:
-      {
-        alert("Please select valid action ");
-        break;
+    if (action == 'decimal'){
+      display.textContent = display.textContent  + '.'
+      action_display.textContent = action_display.textContent + '.'
+      calculator.dataset.previousKeyType = 'deciml'
+    }
+    if (
+      action === 'add' ||
+      action === 'subtract' ||
+      action === 'multiply' ||
+      action === 'divide'
+    ) {
+      key.classList.add('is-depressed')
+      calculator.dataset.previousKeyType = 'operator'
+      firstNumber = display.textContent
+      firstNumber = Number(firstNumber)
+      operator = action
+      if (operator === 'add') {
+        action_display.textContent = action_display.textContent + '+'
       }
+      else if (operator === 'subtract') {
+        action_display.textContent = action_display.textContent + '-'
+      }
+      else if (operator === 'multiply') {
+        action_display.textContent = action_display.textContent + '✕';
+      }
+      else if (operator === 'divide') {
+        action_display.textContent = action_display.textContent + '/'
+      }
+    }
+    if (
+      action === 'calculate'
+    ) {
+      calculator.dataset.previousKeyType = 'operator'
+      Array.from(key.parentNode.children)
+      .forEach(k => k.classList.remove('is-depressed'))
+      secondNumber = display.textContent
+      secondNumber = Number(secondNumber)
+      if (operator === 'add') {
+        result = firstNumber + secondNumber
+      }
+      else if (operator === 'subtract') {
+        result = firstNumber - secondNumber
+      }
+      else if (operator === 'multiply') {
+        result = firstNumber * secondNumber
+      }
+      else if (operator === 'divide') {
+        result = firstNumber / secondNumber
+      }
+      display.textContent = (result).toFixed(3)
+      firstNumber = result
+      action_display.textContent = action_display.textContent  + '=' + display.textContent
+    }
+
+    if (
+      action === 'clear'
+    ) {
+      operator = 'none'
+      firstNumber = 0
+      secondNumber = 0
+      result = 0
+      display.textContent = 0
+      action_display.textContent = 0
+      return
+    }
   }
-  console.log(result);
-}
+ })
